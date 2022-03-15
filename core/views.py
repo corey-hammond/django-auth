8971e6e813efc43c3ec287d4ebfb176e59842722
Corey Hammond <coreyjhammond@gmail.com>
2022-03-15 10:09:38 -0700

    adding UserSerializer

--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
+from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import UserSerializer
+
+
 class RegisterAPIView(APIView):
     def post(self, request):
-        return Response(request.data)
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Passwords do not match')
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
