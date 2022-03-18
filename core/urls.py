2f56e5d1d61a6634ec1038ec76eb4ee93cb266d7
Corey Hammond <coreyjhammond@gmail.com>
2022-03-18 13:40:19 -0700

    two-factor url

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from ast import For
 from django.urls import path, include
-from .views import LogoutAPIView, RefreshAPIView, RegisterAPIView, LoginAPIView, ResetAPIView, UserAPIView, ForgotPasswordAPIView
+from .views import LogoutAPIView, RefreshAPIView, RegisterAPIView, LoginAPIView, ResetAPIView, TwoFactorAPIView, UserAPIView, ForgotPasswordAPIView
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view()),
     path('login', LoginAPIView.as_view()),
+    path('two-factor', TwoFactorAPIView.as_view()),
     path('user', UserAPIView.as_view()),
     path('refresh', RefreshAPIView.as_view()),
     path('logout', LogoutAPIView.as_view()),
