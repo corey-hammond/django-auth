6c0a10301ad8dbbb9c5d877a1659bbd6c77a44d4
Corey Hammond <coreyjhammond@gmail.com>
2022-03-18 11:54:01 -0700

    rewriting login apiview to handle two factor auth using pytop

--- core/models.py
+++ core/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    tfa_secret = models.CharField(max_length=255, default='')
     username = None
 
     USERNAME_FIELD = 'email'
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 import datetime
 import random
 import string
+import pyotp
 from django.core.mail import send_mail
 from rest_framework import exceptions
 from rest_framework.authentication import get_authorization_header
@@ -39,13 +40,46 @@ class LoginAPIView(APIView):
         if not user.check_password(password):
             raise exceptions.AuthenticationFailed('Invalid credentials')
 
+        if user.tfa_secret:
+            return Response({
+                'id': user.id
+            })
+
+        secret = pyotp.random_base32()
+        otpauth_url = pyotp.totp.TOTP(
+            secret).provisioning_uri(issuer_name='My App')
+
+        return Response({
+            'id': user.id,
+            'secret': secret,
+            'otpauth_url': otpauth_url
+        })
+
+
+class TwoFactorAPIView(APIView):
+    def post(self, request):
+        id = request.data['id']
+        user = User.objects.filter(pk=id).first()
+
+        if not user:
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        secret = user.tfa_secret if user.tfa_secret != '' else request.data['secret']
+
+        if not pyotp.TOTP(secret).verify(request.data['code']):
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        if user.tfa_secret == '':
+            user.tfa_secret = secret
+            user.save()
+
         # create JWT access and refresh tokens
-        access_token = create_access_token(user.id)
-        refresh_token = create_refresh_token(user.id)
+        access_token = create_access_token(id)
+        refresh_token = create_refresh_token(id)
 
         # create a UserToken value
         UserToken.objects.create(
-            user_id=user.id,
+            user_id=id,
             token=refresh_token,
             expired_at=datetime.datetime.utcnow() + datetime.timedelta(days=7)
         )
@@ -116,7 +150,7 @@ class ForgotPasswordAPIView(APIView):
         )
 
         # url to frontend
-        url = 'http://localhost:3000/forgot-password/' + token
+        url = 'http://localhost:3000/reset-password/' + token
 
         # send reset email
         send_mail(
